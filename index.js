282c7fd59be9a09fe9de0b38a734f059a0c17297
vijaydev97 <vijaybprajapati06@gmail.com>
2026-06-05 22:51:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Contact = require("./models/contacts.models")
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/contacts-crud')
+.then(() => console.log("database connected"))
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
+
+
+app.get('/',async(req,res) => { 
+    const contacts = await Contact.find()
+    
+    res.render('home',{contacts})
+})
+
+app.get('/show-contact/:id',async(req,res) => {
+    const contact = await Contact.findById( req.params.id)   
+    res.render('show-contact',{ contact})
+    })
+
+app.get('/add-contact',(req,res) => { res.render('add-contact')})
+
+app.post('/add-contact',async(req,res) => {
+    const contact = await Contact.insertOne({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        phone: req.body.phone,
+        address:req.body.address
+
+    })
+    res.redirect("/")
+
+})
+
+app.get('/update-contact/:id',async(req,res) => {
+    const contact = await Contact.findById( req.params.id)   
+    res.render('update-contact',{ contact})
+    
+})
+
+app.post('/update-contact/:id',async (req,res) => {
+    await Contact.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect("/")
+})
+
+
+app.get('/delete-contact/:id',async(req,res) => {
+    await Contact.findByIdAndDelete(req.params.id)
+    res.redirect("/")
+})
+
+
 
 app.listen(3000, () => {
-    console.log('server is running on port 3000')
+    console.log("server started successfully on port 3000.")
 })
-
-app.get('/', (req, res) => {
-    res.send({
-        name: 'vijay',
-        age: 19,
-    })
-})
-
-app.set('view ')
-
-app.get('/About', (req, res) => {
-    res.send('<h1>About page</h1>')
-})
-
-app.get('/gallery', (req, res) => {
-    res.send('<h1>gallery</h1>')
-})
-
-app.get('/user/:userid-:bookid', (req, res) => {
-    res.send(req.params)
-})
-
-app.get('/search', (req, res) => {
-    const name = req.query.name
-    const age = req.query.age
-
-    res.send(`search results - Name : ${name}, Age : ${age}`)
-})
-
---
+++ models/contacts.models.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const contactSchema = mongoose.Schema({
+    first_name:{
+        type: String
+    },
+    last_name:{
+        type: String
+    },
+    email:{
+        type: String
+    },
+    phone:{
+        type: String
+    },
+    address:{
+        type: String
+    },
+})
+
+const contact = mongoose.model("Contact", contactSchema)
+
+module.exports = contact
